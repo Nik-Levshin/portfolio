ad913b3b61abd5993c5d473966afda5175112db5
Nik-Levshin <nikita.levshin2016@gmail.com>
2020-01-31 00:10:12 +0300

    Форма

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,24 @@ var mySwiper = new Swiper('.swiper-container', {
     clickable: true
   },
 });
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
