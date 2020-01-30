dd9b1a4e99ed356c4b465fd424d0e95a37935afa
Nik-Levshin <nikita.levshin2016@gmail.com>
2020-01-30 23:06:04 +0300

    Форма подвал

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-new WOW().init();
-var mySwiper = new Swiper('.swiper-container', {
-  speed: 400,
-  spaceBetween: 100,
-  pagination: {
-    el: '.project-pagination',
-    bulletClass: 'project-bullet',
-    bulletActiveClass: 'project-bullet-active',
-    clickable: true
-  },
-});
-
 $(document).ready(function() {
 
 	//E-mail Ajax Send
